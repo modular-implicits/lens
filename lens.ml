93026d8d9ae825c1ee44a48166175cad0db057c5
Patrick Reader <_@pxeger.com>
2023-08-08 15:14:19 +0100

    MVP: tuple lenses, get, and set

--- lens.ml
+++ lens.ml
@@ -1,1 +1,15 @@
-let hello = "hello"
+open Imp.Any
+open Imp.Control
+
+type ('a, 's) lens = {F : Functor} -> ('a -> 'a F.t) -> ('s -> 's F.t)
+
+let _1 {F: Functor} f (a, b) = F.fmap (fun a' -> (a', b)) (f a)
+let _2 {F: Functor} f (a, b) = F.fmap (fun b' -> (a, b')) (f b)
+
+let set (lens: ('a, 's) lens) (a: 'a) (s: 's) : 's =
+  let Identity s' = lens (fun _ -> Identity a) s
+  in s'
+
+let get {A: Any} (lens: (A.t_for_any, 's) lens) s =
+  let Const a' = lens (fun a -> Const a) s
+  in a'
--- tests/test.ml
+++ tests/test.ml
@@ -1,1 +1,6 @@
-let () = assert (Lens.hello = "hello")
+let () =
+  let open implicit Imp.Any in
+  let open Lens in
+  assert (set _1 3 (1, "hello") = (3, "hello"));
+  assert (get _1 (1, "hello") = 1);
+  assert (get _2 (1, "hello") = "hello")
