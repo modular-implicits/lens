3735387e297b6c13bdef82c64d582812f4867423
Patrick Reader <_@pxeger.com>
2023-08-09 10:11:13 +0100

    support type changes (Lens s t a b instead of Lens s a)

--- lens.ml
+++ lens.ml
@@ -1,15 +1,15 @@
 open Imp.Any
 open Imp.Control
 
-type ('a, 's) lens = {F : Functor} -> ('a -> 'a F.t) -> ('s -> 's F.t)
+type ('s, 't, 'a, 'b) lens = {F : Functor} -> ('a -> 'b F.t) -> ('s -> 't F.t)
 
 let _1 {F: Functor} f (a, b) = F.fmap (fun a' -> (a', b)) (f a)
 let _2 {F: Functor} f (a, b) = F.fmap (fun b' -> (a, b')) (f b)
 
-let set (lens: ('a, 's) lens) (a: 'a) (s: 's) : 's =
-  let Identity s' = lens (fun _ -> Identity a) s
-  in s'
+let set (lens: ('s, 't, 'a, 'b) lens) (b: 'b) (s: 's) : 't =
+  let Identity t = lens (fun _ -> Identity b) s
+  in t
 
-let get {A: Any} (lens: (A.t_for_any, 's) lens) s =
+let get {A: Any} (lens: ('s, 't, A.t_for_any, A.t_for_any) lens) s =
   let Const a' = lens (fun a -> Const a) s
   in a'
--- tests/test.ml
+++ tests/test.ml
@@ -1,6 +1,6 @@
 let () =
   let open implicit Imp.Any in
   let open Lens in
-  assert (set _1 3 (1, "hello") = (3, "hello"));
+  assert (set _1 "lol" (1, "hello") = ("lol", "hello"));
   assert (get _1 (1, "hello") = 1);
   assert (get _2 (1, "hello") = "hello")
