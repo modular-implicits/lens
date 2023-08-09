d3cc162524dc3c5b7ae39924089dcdd777e76b71
Patrick Reader <_@pxeger.com>
2023-08-09 16:45:55 +0100

    generalise get and set to eliminate gett and sett

--- lens.ml
+++ lens.ml
@@ -2,8 +2,76 @@ open Imp.Any
 open Imp.Control
 open Imp.Data
 
+(* TYPE SETUP AND COMBINATORS *)
+
 type ('s, 't, 'a, 'b) lens = {F : Functor} -> ('a -> 'b F.t) -> ('s -> 't F.t)
 type ('s, 't, 'a, 'b) traversal = {F : Applicative} -> ('a -> 'b F.t) -> ('s -> 't F.t)
+
+type ('a, 's, 'x) getter = ('x -> ('a, 'x) const) -> ('s -> ('a, 's) const)
+
+module type Getter = sig
+  type ('s, 't, 'a, 'b) t
+  type a
+  val convert : ('s, 's, a, a) t -> (a, 's, a) getter
+end
+
+implicit module Lens_Getter {A: Any} : Getter
+  with type ('s, 't, 'a, 'b) t = {F: Functor} -> ('a -> 'b F.t) -> ('s -> 't F.t)
+  and type a = A.t_for_any
+= struct
+  type ('s, 't, 'a, 'b) t = {F: Functor} -> ('a -> 'b F.t) -> ('s -> 't F.t)
+  type a = A.t_for_any
+  let convert (l: ('s, 't, a, a) t): (a, 's, a) getter =
+    l {Const {A}}
+end
+
+implicit module Traversal_Getter {A: Monoid} : Getter
+  with type ('s, 't, 'a, 'b) t = {F: Applicative} -> ('a -> 'b F.t) -> ('s -> 't F.t)
+  and type a = A.t
+= struct
+  type ('s, 't, 'a, 'b) t = {F: Applicative} -> ('a -> 'b F.t) -> ('s -> 't F.t)
+  type a = A.t
+  let convert (l: ('s, 't, a, a) t): (a, 's, a) getter =
+    l {Const_Applicative {A}}
+end
+
+let get {L: Getter} (lens: ('s, 't, L.a, L.a) L.t) s =
+  let Const a' = L.convert lens (fun a -> Const a) s
+  in a'
+
+(* (l >< m) f = l (m (f)) *)
+let (><) (l: ('s, 't, 'x, 'y) lens) (m: ('x, 'y, 'a, 'b) lens) : ('s, 't, 'a, 'b) lens =
+  fun {F: Functor} f -> l (m f)
+
+type ('s, 't, 'a, 'b) setter = ('a -> 'b identity) -> ('s -> 't identity)
+
+module type Setter = sig
+  type ('s, 't, 'a, 'b) t
+  val convert : ('s, 't, 'a, 'b) t -> ('s, 't, 'a, 'b) setter
+end
+
+implicit module Lens_Setter : Setter
+  with type ('s, 't, 'a, 'b) t = {F: Functor} -> ('a -> 'b F.t) -> ('s -> 't F.t)
+= struct
+  type ('s, 't, 'a, 'b) t = {F: Functor} -> ('a -> 'b F.t) -> ('s -> 't F.t)
+  let convert (l: ('s, 't, 'a, 'b) t): ('s, 't, 'a, 'b) setter = l {Identity}
+end
+
+implicit module Traversal_Setter : Setter
+  with type ('s, 't, 'a, 'b) t = {F: Applicative} -> ('a -> 'b F.t) -> ('s -> 't F.t)
+= struct
+  type ('s, 't, 'a, 'b) t = {F: Applicative} -> ('a -> 'b F.t) -> ('s -> 't F.t)
+  let convert (l: ('s, 't, 'a, 'b) t): ('s, 't, 'a, 'b) setter = l {Identity}
+end
+
+let set {L: Setter} (l: ('s, 't, 'a, 'b) L.t) (b: 'b) (s: 's) : 't =
+  let Identity t = L.convert l (fun _ -> Identity b) s
+  in t
+
+(* THE LENSES THEMSELVES *)
+
+let traversed {T: Traversable} : ('a T.t, 'b T.t, 'a, 'b) traversal =
+  fun {A: Applicative} f -> T.traverse f
 
 module T2 = struct
   let _1 {F: Functor} f (a, b) = F.fmap (fun a' -> (a', b)) (f a)
@@ -22,30 +90,3 @@ module T4 = struct
   let _3 {F: Functor} f (a, b, c, d) = F.fmap (fun c' -> (a, b, c', d)) (f c)
   let _4 {F: Functor} f (a, b, c, d) = F.fmap (fun d' -> (a, b, c, d')) (f d)
 end
-
-let get (type a) (lens: ('s, 't, a, a) lens) s =
-  let implicit module A: Any with type t_for_any = a
-    = struct type t_for_any = a end in
-  let Const a' = lens (fun a -> Const a) s
-  in a'
-
-let gett {A: Monoid} (lens: ('s, 't, A.t, A.t) traversal) s =
-  let implicit module A: Any with type t_for_any = A.t
-    = struct type t_for_any = A.t end in
-  let Const a' = lens (fun a -> Const a) s
-  in a'
-
-(* (l >< m) f = l (m (f)) *)
-let (><) (l: ('s, 't, 'x, 'y) lens) (m: ('x, 'y, 'a, 'b) lens) : ('s, 't, 'a, 'b) lens =
-  fun {F: Functor} f -> l (m f)
-
-let set (l: ('s, 't, 'a, 'b) lens) (b: 'b) (s: 's) : 't =
-  let Identity t = l {Identity} (fun _ -> Identity b) s
-  in t
-
-let sett (l: ('s, 't, 'a, 'b) traversal) (b: 'b) (s: 's) : 't =
-  let Identity t = l (fun _ -> Identity b) s
-  in t
-
-let traversed {T: Traversable} : ('a T.t, 'b T.t, 'a, 'b) traversal =
-  fun {A: Applicative} f -> T.traverse f
--- tests/test.ml
+++ tests/test.ml
@@ -1,4 +1,5 @@
 let () =
+  let open implicit Imp.Any in
   let open implicit Imp.Data in
   let open Lens in
   assert (set T2._1 "lol" (1, "hello") = ("lol", "hello"));
@@ -6,5 +7,5 @@ let () =
   assert (get T2._2 (1, "hello") = "hello");
   assert (get (T2._1 >< T2._2) ((5, "lol"), "hello") = "lol");
   assert (set (T2._1 >< T2._2) 7 ((5, "lol"), "hello") = ((5, 7), "hello"));
-  assert (sett (traversed {Imp.Control.List}) 5 [1;2;3;4] = [5;5;5;5]);
-  assert (gett (traversed {Imp.Control.List}) ["hi"; "there"] = "hithere")
+  assert (set (traversed {Imp.Control.List}) 5 [1;2;3;4] = [5;5;5;5]);
+  assert (get (traversed {Imp.Control.List}) ["hi"; "there"] = "hithere")
