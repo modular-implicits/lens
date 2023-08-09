4982e30dd794d8109c0592439a0e1619f42440ea
Patrick Reader <_@pxeger.com>
2023-08-09 15:13:52 +0100

    add traversals

--- lens.ml
+++ lens.ml
@@ -1,7 +1,9 @@
 open Imp.Any
 open Imp.Control
+open Imp.Data
 
 type ('s, 't, 'a, 'b) lens = {F : Functor} -> ('a -> 'b F.t) -> ('s -> 't F.t)
+type ('s, 't, 'a, 'b) traversal = {F : Applicative} -> ('a -> 'b F.t) -> ('s -> 't F.t)
 
 module T2 = struct
   let _1 {F: Functor} f (a, b) = F.fmap (fun a' -> (a', b)) (f a)
@@ -21,16 +23,29 @@ module T4 = struct
   let _4 {F: Functor} f (a, b, c, d) = F.fmap (fun d' -> (a, b, c, d')) (f d)
 end
 
-let set (lens: ('s, 't, 'a, 'b) lens) (b: 'b) (s: 's) : 't =
-  let Identity t = lens (fun _ -> Identity b) s
-  in t
-
 let get (type a) (lens: ('s, 't, a, a) lens) s =
   let implicit module A: Any with type t_for_any = a
     = struct type t_for_any = a end in
   let Const a' = lens (fun a -> Const a) s
   in a'
 
+let gett {A: Monoid} (lens: ('s, 't, A.t, A.t) traversal) s =
+  let implicit module A: Any with type t_for_any = A.t
+    = struct type t_for_any = A.t end in
+  let Const a' = lens (fun a -> Const a) s
+  in a'
+
 (* (l >< m) f = l (m (f)) *)
 let (><) (l: ('s, 't, 'x, 'y) lens) (m: ('x, 'y, 'a, 'b) lens) : ('s, 't, 'a, 'b) lens =
   fun {F: Functor} f -> l (m f)
+
+let set (l: ('s, 't, 'a, 'b) lens) (b: 'b) (s: 's) : 't =
+  let Identity t = l {Identity} (fun _ -> Identity b) s
+  in t
+
+let sett (l: ('s, 't, 'a, 'b) traversal) (b: 'b) (s: 's) : 't =
+  let Identity t = l (fun _ -> Identity b) s
+  in t
+
+let traversed {T: Traversable} : ('a T.t, 'b T.t, 'a, 'b) traversal =
+  fun {A: Applicative} f -> T.traverse f
