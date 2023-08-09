cbe7e9ba675c06cd72fabd016f00e440b6f83041
Patrick Reader <_@pxeger.com>
2023-08-09 10:23:00 +0100

    add composition

--- lens.ml
+++ lens.ml
@@ -13,3 +13,7 @@ let set (lens: ('s, 't, 'a, 'b) lens) (b: 'b) (s: 's) : 't =
 let get {A: Any} (lens: ('s, 't, A.t_for_any, A.t_for_any) lens) s =
   let Const a' = lens (fun a -> Const a) s
   in a'
+
+(* (l >< m) f = l (m (f)) *)
+let (><) (l: ('s, 't, 'x, 'y) lens) (m: ('x, 'y, 'a, 'b) lens) : ('s, 't, 'a, 'b) lens =
+  fun {F: Functor} f -> l (m f)
--- tests/test.ml
+++ tests/test.ml
@@ -3,4 +3,6 @@ let () =
   let open Lens in
   assert (set _1 "lol" (1, "hello") = ("lol", "hello"));
   assert (get _1 (1, "hello") = 1);
-  assert (get _2 (1, "hello") = "hello")
+  assert (get _2 (1, "hello") = "hello");
+  assert (get (_1 >< _2) ((5, "lol"), "hello") = "lol");
+  assert (set (_1 >< _2) 7 ((5, "lol"), "hello") = ((5, 7), "hello"))
