07a82bfd886d980a48b33d1aec1542d326bbcb14
Patrick Reader <_@pxeger.com>
2023-08-09 10:30:49 +0100

    add lenses for 3- and 4-tuples

--- lens.ml
+++ lens.ml
@@ -3,8 +3,23 @@ open Imp.Control
 
 type ('s, 't, 'a, 'b) lens = {F : Functor} -> ('a -> 'b F.t) -> ('s -> 't F.t)
 
-let _1 {F: Functor} f (a, b) = F.fmap (fun a' -> (a', b)) (f a)
-let _2 {F: Functor} f (a, b) = F.fmap (fun b' -> (a, b')) (f b)
+module T2 = struct
+  let _1 {F: Functor} f (a, b) = F.fmap (fun a' -> (a', b)) (f a)
+  let _2 {F: Functor} f (a, b) = F.fmap (fun b' -> (a, b')) (f b)
+end
+
+module T3 = struct
+  let _1 {F: Functor} f (a, b, c) = F.fmap (fun a' -> (a', b, c)) (f a)
+  let _2 {F: Functor} f (a, b, c) = F.fmap (fun b' -> (a, b', c)) (f b)
+  let _3 {F: Functor} f (a, b, c) = F.fmap (fun c' -> (a, b, c')) (f c)
+end
+
+module T4 = struct
+  let _1 {F: Functor} f (a, b, c, d) = F.fmap (fun a' -> (a', b, c, d)) (f a)
+  let _2 {F: Functor} f (a, b, c, d) = F.fmap (fun b' -> (a, b', c, d)) (f b)
+  let _3 {F: Functor} f (a, b, c, d) = F.fmap (fun c' -> (a, b, c', d)) (f c)
+  let _4 {F: Functor} f (a, b, c, d) = F.fmap (fun d' -> (a, b, c, d')) (f d)
+end
 
 let set (lens: ('s, 't, 'a, 'b) lens) (b: 'b) (s: 's) : 't =
   let Identity t = lens (fun _ -> Identity b) s
--- tests/test.ml
+++ tests/test.ml
@@ -1,8 +1,8 @@
 let () =
   let open implicit Imp.Any in
   let open Lens in
-  assert (set _1 "lol" (1, "hello") = ("lol", "hello"));
-  assert (get _1 (1, "hello") = 1);
-  assert (get _2 (1, "hello") = "hello");
-  assert (get (_1 >< _2) ((5, "lol"), "hello") = "lol");
-  assert (set (_1 >< _2) 7 ((5, "lol"), "hello") = ((5, 7), "hello"))
+  assert (set T2._1 "lol" (1, "hello") = ("lol", "hello"));
+  assert (get T2._1 (1, "hello") = 1);
+  assert (get T2._2 (1, "hello") = "hello");
+  assert (get (T2._1 >< T2._2) ((5, "lol"), "hello") = "lol");
+  assert (set (T2._1 >< T2._2) 7 ((5, "lol"), "hello") = ((5, 7), "hello"))
